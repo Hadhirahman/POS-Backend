b11fdfbbd94ca5b461e430473598d3d3b64eab58
hadhi rahman <hadhirahmanmt@gmail.com>
2024-04-02 12:41:54 +0530

    console added in database config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ const mongoose=require("mongoose")
 const connect=mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("database is connected")
 })
-.catch(()=>{
+.catch((error)=>{
+    console.error(TypeError);
     console.log("data base not connected");
 })
 module.exports=connect
