281915e244b106c2d399e6f45152e3e6b2e30587
hadhi rahman <hadhirahmanmt@gmail.com>
2024-04-02 12:44:10 +0530

    error

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const connect=mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("database is connected")
 })
 .catch((error)=>{
-    console.error(TypeError);
+    console.error(error);
     console.log("data base not connected");
 })
 module.exports=connect
